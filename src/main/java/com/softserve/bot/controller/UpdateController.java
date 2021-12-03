dbe8439f8c22ae7625f79b42cb9ed8b14cdc48e7
DEEVERee <dbrbyu369@gmail.com>
2021-12-03 17:29:02 +0200

    The conflicts were resolved

--- src/main/java/com/softserve/bot/controller/UpdateController.java
+++ src/main/java/com/softserve/bot/controller/UpdateController.java
@@ -1,14 +1,16 @@
-package com.softserve.webhookbot.controller;
+package com.softserve.bot.controller;
 
-import com.softserve.webhookbot.entity.WebhookBot;
-import com.softserve.webhookbot.enumeration.Subject;
-import com.softserve.webhookbot.util.EnumSetUtil;
+import com.softserve.bot.model.Subject;
+import com.softserve.bot.util.EnumSetUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
