67e88a04a60aa6a10da6a721470c608bf756164d
DEEVERee <dbrbyu369@gmail.com>
2021-12-16 22:13:03 +0200

    RestController for getting divided branches was implemented, exception handler was added.

--- src/main/java/com/softserve/bot/controller/BranchController.java
+++ src/main/java/com/softserve/bot/controller/BranchController.java
@@ -2,23 +2,16 @@ package com.softserve.bot.controller;
 
 import com.softserve.bot.dto.BranchDto;
 import com.softserve.bot.dto.SpecialtyDTO;
-import com.softserve.bot.exception.BranchControllerException;
 import com.softserve.bot.exception.SpecialtyNotFoundException;
-import com.softserve.bot.model.Specialty;
+import com.softserve.bot.exception.TypeBranchException;
 import com.softserve.bot.model.BranchOfKnowledge;
 import com.softserve.bot.service.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.ext.ParamConverter;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,6 +20,7 @@ import java.util.stream.Collectors;
 public class BranchController {
     private final Filter filter;
     private List<BranchDto> branchDto;
+    private List<String> typeBranchList;
 
     @GetMapping("/{id}/specialties")
     public ResponseEntity<List<BranchDto>> findSpecialtiesByBranchId(@PathVariable String id){
@@ -49,5 +43,21 @@ public class BranchController {
     @GetMapping
     public ResponseEntity<List<BranchOfKnowledge>> getBranches(){
         return new ResponseEntity<List<BranchOfKnowledge>>(filter.getBranchesOfKnowledge(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<List<String>> getBranchesDividedOnType(@PathVariable String type) {
+        if (type.equalsIgnoreCase("hum")) {
+            typeBranchList = filter.getBranchesOfKnowledgeByType("Гуманітарні").stream()
+                    .map(BranchOfKnowledge::toString)
+                    .collect(Collectors.toList());
+        } else if (type.equalsIgnoreCase("tech")) {
+            typeBranchList = filter.getBranchesOfKnowledgeByType("Технічні").stream()
+                    .map(BranchOfKnowledge::toString)
+                    .collect(Collectors.toList());
+        } else {
+            throw new TypeBranchException();
+        }
+        return new ResponseEntity<>(typeBranchList, HttpStatus.OK);
     }
 }
